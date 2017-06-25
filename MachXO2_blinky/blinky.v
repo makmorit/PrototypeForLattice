2c98b44e62a84d7e805112169c007c076a317131
Makoto Morita <makmorit@outlook.jp>
2017-06-25 17:59:18 +0900

    不具合修正

--- MachXO2_blinky/blinky.v
+++ MachXO2_blinky/blinky.v
@@ -22,23 +22,22 @@ module blinky (
     // counter with no flag control
     always @ (posedge osc_clk) begin
         if (count < {8'h 78, {N-7{1'h 0}}}) begin
-            count <= count + {N{1'h 1}};
+            count <= count + {1'h 1};
         end else begin
             count <= {N{1'h 0}};
         end
     end
 
-    //4-8 data encoder ROM
     always @ (count[N-1:N-2]) begin
         case (count[N-1:N-2])
             2'b 01:
-                LED[3:0] <= ~4'b 1000;
+                LED[3:0] <= 4'b 1000;
             2'b 10:
-                LED[3:0] <= ~4'b 0100;
+                LED[3:0] <= 4'b 0100;
             2'b 11:
-                LED[3:0] <= ~4'b 0010;
+                LED[3:0] <= 4'b 0010;
             default :
-                LED[3:0] <= ~4'b 0001;
+                LED[3:0] <= 4'b 0001;
         endcase
     end
 endmodule
